7bc1d81035ff424296620e5fe4ebd08ce742dbde
w-r d-a <wiktor.skc@gmail.com>
2023-12-02 09:54:04 +0100

    Created files

---
+++ 2023/day01.c
@@ -0,0 +1,15 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>", argv[0]);
+        return 1;
+    }
+    FILE* file = fopen(argv[1], "r");
+    if !(file) {
+        fprintf(stderr, "Could not open file %s: %s", argv[1], strerror(errno));
+        return 1;
+    }
+}
